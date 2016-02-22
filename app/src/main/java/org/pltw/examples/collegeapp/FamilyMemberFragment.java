a7a13e2cb359532a502e8ffe9f5b1c9f7ef74e87
PLTW <ianlevasseur12@gmail.com>
2016-02-22 09:10:41 -0600

    Initial commit

---
+++ app/src/main/java/org/pltw/examples/collegeapp/FamilyMemberFragment.java
@@ -0,0 +1,17 @@
+package org.pltw.examples.collegeapp;
+
+import android.support.v4.app.Fragment;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public abstract class FamilyMemberFragment extends Fragment {
+
+    private static final String TAG = "FamilyMemberFragment";
+
+    protected TextView mFirstName;
+    protected EditText mEnterFirstName;
+    protected TextView mLastName;
+    protected EditText mEnterLastName;
+
+}
